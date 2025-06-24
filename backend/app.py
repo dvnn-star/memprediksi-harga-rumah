7a4a16743c11ba0dede787ffd011de643b4a2814
dvnn-star <179110642+dvnn-star@users.noreply.github.com>
2025-06-24 16:33:14 +0700

    api

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,11 @@ import numpy as np
 import joblib
 from flask_cors import CORS
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_DIR = os.path.join(BASE_DIR, 'model')
@@ -20,21 +25,27 @@ model = joblib.load(os.path.join(MODEL_DIR, 'model.pkl'))
 scaler = joblib.load(os.path.join(MODEL_DIR, 'scaler.pkl'))
 features = joblib.load(os.path.join(MODEL_DIR, 'features.pkl'))
 @app.route ('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     try:
-        data= request.get_json()
+        key = request.headers.get('X-API-KEY')
+        if key != API_KEY:
+            return jsonify({'error': 'Unauthorized'}), 401
+
+        data = request.get_json()
         df = pd.DataFrame([data])
-        df =df[features]
+        df = df[features]
         df = df.astype(float)
         df_scaled = scaler.transform(df)
         df_scaled = pd.DataFrame(df_scaled, columns=features, index=df.index)
-    
+
         prediction = model.predict(df_scaled)
         result = np.expm1(prediction[0])
         return jsonify({'prediction': round(result, 2)})
-        
+
     except Exception as e:
         return jsonify({'error': str(e)}), 400
+
 
 if __name__ == '__main__':
     app.run(debug=True)
